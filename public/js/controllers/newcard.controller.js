e7b78bf4c27e7caf04233012716ceaa76b36b7ad
Daniel Moennich <daniel.moennich@gmail.com>
2015-08-28 15:00:43 -0400

    Added form validation.

--- public/js/controllers/newcard.controller.js
+++ public/js/controllers/newcard.controller.js
@@ -11,6 +11,22 @@ app.controller("NewCardController", function ($scope, FlashCardsFactory) {
 	};
 
 	$scope.makeNewCard = function(){
+
+		var filledAnswers = $scope.newCard.answers.filter(function (answer) {
+			return answer.text !== null;
+		});
+		var answerCount = filledAnswers.length;
+
+		var correct = filledAnswers.some(function (answer) {
+			return answer.correct;
+		});
+
+		if ($scope.newCardForm.$invalid || !correct || answerCount === 0) {
+			console.log("Form not valid!!");
+			return;
+		}
+
+		console.log("Form seems valid");
 		FlashCardsFactory.makeFlashCard($scope.newCard).then(function(card){
 			console.log('Card created:',card);
 		}).catch(function(err){
